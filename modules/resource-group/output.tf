77f1960b64369c907bf9c6ac55d2582a3f03c18d
singhsynergy <root@CS-JASWSING-LT.clsz.local>
2026-02-26 07:50:48 +0000

    Initial release

--- modules/resource-group/output.tf
+++ modules/resource-group/output.tf
@@ -5,10 +5,10 @@ output "id" {
 
 output "name" {
   description = "The name of the Resource Group"
-  value       = azurerm_resource_group.rp.name
+  value       = azurerm_resource_group.rg.name
 }
 
 output "location" {
   description = "The location of the Resource Group"
-  value       = azurerm_resource_group.rp.location
+  value       = azurerm_resource_group.rg.location
 }
